d8c79cc5beb8020fa6782a0439b6591f6ce4059b
Audrey Behiels <audrey.behiels@student.hogent.be>
2020-02-25 18:04:09 +0100

    DCD updated #4 + ISessieRepository added

---
+++ dotnet-g36/Models/UserSessie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotnet_g36
+{
+    public class UserSessie
+    {
+        public Sessie Sessie
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public User User
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
