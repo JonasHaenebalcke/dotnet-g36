09678a7ee6e6bee57f135949db94c6216175714c
Jonas Haenebalcke <jonas.haenebalcke@student.hogent.be>
2020-02-25 18:18:53 +0100

    Start Databank #20

---
+++ dotnet-g36/Models/SessieVerantwoordelijke.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_g36.Models
+{
+    public class SessieVerantwoordelijke
+    {
+        //Zie SessieHoofdVerantwoordelijke
+    }
+}
