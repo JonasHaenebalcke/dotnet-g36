745156917d7f2b9ef89d3986ca5986ffd1f6ddfa
Audrey Behiels <audrey.behiels@student.hogent.be>
2020-02-25 18:04:09 +0100

    DCD updated #4 + ISessieRepository added

---
+++ dotnet-g36/Models/User.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace dotnet_g36
+{
+    public class User : object
+    {
+
+        public String Voornaam
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public String Familienaam
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public List<UserSessie> UserSessie
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int UserID
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public String GebruikersNaam
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public String Wachtwoord
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
