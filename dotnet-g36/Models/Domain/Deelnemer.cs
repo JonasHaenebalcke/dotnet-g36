ba396dc45cdf0512684dd62212758d3330bece13
Jonas Haenebalcke <jonas.haenebalcke@student.hogent.be>
2020-03-02 16:57:44 +0100

    Lijsten initialiseren in constructor en methodes van sessie veranderd naar deelnemer #23

--- dotnet-g36/Models/Domain/Deelnemer.cs
+++ dotnet-g36/Models/Domain/Deelnemer.cs
@@ -4,12 +4,39 @@ namespace dotnet_g36.Models.Domain
     public class Deelnemer : User
     {
         #region constructors
-        public Deelnemer() :base()
+        public Deelnemer(string voornaam, string familienaam,  StatusGebruiker statusGebruiker) : base(voornaam, familienaam, statusGebruiker)
         {
         }
+        #endregion
 
-        public Deelnemer(string voornaam, string familienaam, /*int userID,*/ StatusGebruiker statusGebruiker) : base(voornaam, familienaam,/* userID,*/ statusGebruiker)
+        #region Methods
+        public bool MeldAanwezig(int sessieID, int userID)
         {
+            //     Boolean us = UserSessies.Where(s => s.UserID.Equals(userID)).FirstOrDefault().Ingeschreven();
+
+            //// Als gebruiker is ingeschreven en niet in de lijst van aanwezigen zit, steek in lijst aanwezigen en return true;
+
+            return false;
+            //throw new System.NotImplementedException();
+        }
+
+        public bool SchrijfIn(int sessieID, int userID)
+        {
+            // als gebruiker nog niet is ingeschreven dan gebruiker ingeschrijven
+            // if(Ingeschrevenen.Contains() )
+            throw new System.NotImplementedException();
+        }
+
+        public bool SchrijfUit(int sessieID, int userID)
+        {
+            // als gebruiker nog is ingeschreven dan gebruiker uitschrijven
+            throw new System.NotImplementedException();
+        }
+
+        public void FeedbackGeven()
+        {
+            // kan alleen als ingeschreven en aanwezig was
+            throw new System.NotImplementedException();
         }
         #endregion
     }
