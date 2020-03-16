4e162792e048c13b0472b05febb5c5016c11410b
rein <rein.daelman@student.hogent.be>
2020-03-16 12:35:27 +0100

    MeldAanwezigViewModel

---
+++ dotnet-g36/Models/ViewModels/MeldAanwezigViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using dotnet_g36.Models.Domain;
+
+namespace dotnet_g36.Models.ViewModels
+{
+    public class MeldAanwezigViewModel
+    {
+        public String Barcode { get; set; }
+        public Sessie sessie;
+        public MeldAanwezigViewModel() {}
+        public MeldAanwezigViewModel(Sessie sessie)
+        {
+            this.sessie = sessie;
+        }
+
+        
+    }
+}
